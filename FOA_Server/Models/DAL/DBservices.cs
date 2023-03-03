e4ca3b327cd38df910776a1e7bb86a5d59c79bd9
Lotem <lotemwolb@gmail.com>
2023-03-03 12:18:28 +0200

    add VolunteerProgram + Post
    Added VolunteerProgram model + VolunteerProgram controller + VolunteerProgram in DAL.
    Added Post model.

--- FOA_Server/Models/DAL/DBservices.cs
+++ FOA_Server/Models/DAL/DBservices.cs
@@ -31,6 +31,7 @@ public class DBservices
         return con;
     }
 
+    // USERS
     // This method reads all the Users
     public List<User> ReadUsers()
     {
@@ -133,89 +134,6 @@ public class DBservices
 
     }
 
-    // This method inserts a flat to the flat table 
-    /* public int InsertFlt(Flat flat)
-    {
-        SqlConnection con;
-        SqlCommand cmd;
-
-        try
-        {
-            con = connect("myProjDB"); // create the connection
-        }
-        catch (Exception ex)
-        {
-            // write to log
-            throw (ex);
-        }
-
-        String cStr = BuildInsertCommand(flat);      // helper method to build the insert string
-
-        cmd = CreateCommandWithStoredProcedureInsert("spInsertFlat", con, flat);             // create the command
-
-        try
-        {
-            int numEffected = cmd.ExecuteNonQuery(); // execute the command
-            return numEffected;
-        }
-        catch (Exception ex)
-        {
-            // write to log
-            throw (ex);
-        }
-
-        finally
-        {
-            if (con != null)
-            {
-                // close the db connection
-                con.Close();
-            }
-        }
-
-    }
-
-    // This method inserts a vaca to the vaca table 
-    public int InsertVaca(Vacation vaca)
-    {
-        SqlConnection con;
-        SqlCommand cmd;
-
-        try
-        {
-            con = connect("myProjDB"); // create the connection
-        }
-        catch (Exception ex)
-        {
-            // write to log
-            throw (ex);
-        }
-
-        String cStr = BuildInsertCommand(vaca);      // helper method to build the insert string
-
-        cmd = CreateCommandWithStoredProcedureInsert("spInsertVacation", con, vaca);             // create the command
-
-        try
-        {
-            int numEffected = cmd.ExecuteNonQuery(); // execute the command
-            return numEffected;
-        }
-        catch (Exception ex)
-        {
-            // write to log
-            throw (ex);
-        }
-
-        finally
-        {
-            if (con != null)
-            {
-                // close the db connection
-                con.Close();
-            }
-        }
-    } */
-
     // This method update a user to the user table 
     public int UpdateUser(User user)
     {
@@ -255,193 +173,6 @@ public class DBservices
             }
         }
     }
-
-    // This method update a vacation to the vacation table 
-    /* public int UpdateVaca(Vacation vaca)
-    {
-        SqlConnection con;
-        SqlCommand cmd;
-
-        try
-        {
-            con = connect("myProjDB"); // create the connection
-        }
-        catch (Exception ex)
-        {
-            // write to log
-            throw (ex);
-        }
-
-        cmd = CreateCommandWithStoredProcedureUpdate("spUpdateVacation", con, vaca);     // create the command
-
-        try
-        {
-            int numEffected = cmd.ExecuteNonQuery();  // execute the command
-            return numEffected;
-        }
-        catch (Exception ex)
-        {
-            // write to log
-            throw (ex);
-        }
-
-        finally
-        {
-            if (con != null)
-            {
-                // close the db connection
-                con.Close();
-            }
-        }
-
-    }
-
-    // This method update a flat to the flat table 
-    public int UpdateFlat(Flat flat)
-    {
-        SqlConnection con;
-        SqlCommand cmd;
-
-        try
-        {
-            con = connect("myProjDB"); // create the connection
-        }
-        catch (Exception ex)
-        {
-            // write to log
-            throw (ex);
-        }
-
-        cmd = CreateCommandWithStoredProcedureInsert("spUpdateFlat", con, flat);     // create the command
-
-        try
-        {
-            int numEffected = cmd.ExecuteNonQuery();  // execute the command
-            return numEffected;
-        }
-        catch (Exception ex)
-        {
-            // write to log
-            throw (ex);
-        }
-
-        finally
-        {
-            if (con != null)
-            {
-                // close the db connection
-                con.Close();
-            }
-        }
-
-    } */
-
-
-    // This method reads all the Flats
-    /* public List<Flat> ReadFlats()
-    {
-        SqlConnection con;
-        SqlCommand cmd;
-
-        try
-        {
-            con = connect("myProjDB"); // create the connection
-        }
-        catch (Exception ex)
-        {
-            // write to log
-            throw (ex);
-        }
-
-        cmd = CreateCommandWithStoredProcedureRead("spReadFlats", con);      // create the command
-
-        List<Flat> list = new List<Flat>();
-
-        try
-        {
-            SqlDataReader dataReader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
-
-            while (dataReader.Read())
-            {
-                Flat f = new Flat();
-                f.Id = Convert.ToInt32(dataReader["Id"]);
-                f.City = dataReader["City"].ToString();
-                f.Address = dataReader["Address"].ToString();
-                f.NumberOfRooms = Convert.ToInt32(dataReader["NumberOfRooms"]);
-                f.Price = Convert.ToInt32(dataReader["Price"]);
-
-                list.Add(f);
-            }
-            return list;
-        }
-        catch (Exception ex)
-        {
-            // write to log
-            throw (ex);
-        }
-
-        finally
-        {
-            if (con != null)
-            {
-                // close the db connection
-                con.Close();
-            }
-        }
-    }
-
-    // This method reads all the Vacations
-    public List<Vacation> ReadVacations()
-    {
-        SqlConnection con;
-        SqlCommand cmd;
-
-        try
-        {
-            con = connect("myProjDB"); // create the connection
-        }
-        catch (Exception ex)
-        {
-            // write to log
-            throw (ex);
-        }
-
-        cmd = CreateCommandWithStoredProcedureRead("spReadVacations", con);      // create the command
-
-        List<Vacation> list = new List<Vacation>();
-
-        try
-        {
-            SqlDataReader dataReader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
-
-            while (dataReader.Read())
-            {
-                Vacation v = new Vacation();
-                v.Id = Convert.ToInt32(dataReader["Id"]);
-                v.UserId = (dataReader["UserId"]).ToString();
-                v.FlatId = Convert.ToInt32(dataReader["FlatId"]);
-                v.StartDate = Convert.ToDateTime(dataReader["StartDate"]);
-                v.EndDate = Convert.ToDateTime(dataReader["EndDate"]);
-
-                list.Add(v);
-            }
-            return list;
-        }
-        catch (Exception ex)
-        {
-            // write to log
-            throw (ex);
-        }
-
-        finally
-        {
-            if (con != null)
-            {
-                // close the db connection
-                con.Close();
-            }
-        }
-    } */
 
 
     // Create the SqlCommand using a stored procedure for Read
@@ -486,7 +217,7 @@ public class DBservices
 
         return cmd;
     }
-  
+
     private SqlCommand CreateCommandWithStoredProcedureUpdate(String spName, SqlConnection con, User user)
     {
         SqlCommand cmd = new SqlCommand(); // create the command object
@@ -514,5 +245,57 @@ public class DBservices
     }
 
 
-   
+    // Volunteer Programs
+    // This method reads all Volunteer Programs
+    public List<VolunteerProgram> ReadVolunteerPrograms()
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+
+        try
+        {
+            con = connect("myProjDB"); // create the connection
+        }
+        catch (Exception ex)
+        {
+            // write to log
+            Console.WriteLine("Error");
+            throw (ex);
+        }
+
+        cmd = CreateCommandWithStoredProcedureRead("spReadUsers", con);      // create the command
+
+        List<VolunteerProgram> list = new List<VolunteerProgram>();
+
+        try
+        {
+            SqlDataReader dataReader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+            while (dataReader.Read())
+            {
+                VolunteerProgram vp = new VolunteerProgram();
+                vp.ProgramID = Convert.ToInt32(dataReader["ProgramID"]);
+                vp.ProgramName = dataReader["ProgramName"].ToString();
+
+                list.Add(vp);
+            }
+            return list;
+        }
+        catch (Exception ex)
+        {
+            // write to log
+            Console.WriteLine("Error");
+            throw (ex);
+        }
+
+        finally
+        {
+            if (con != null)
+            {
+                // close the db connection
+                con.Close();
+            }
+        }
+    }
+
 }
---
+++ FOA_Server/Models/VolunteerProgram.cs
@@ -0,0 +1,23 @@
+﻿namespace FOA_Server.Models
+{
+    public class VolunteerProgram
+    {
+        public int ProgramID { get; set; }
+        public string ProgramName { get; set; }
+
+        public VolunteerProgram() { }
+        public VolunteerProgram(int programID, string programName)
+        {
+            ProgramID = programID;
+            ProgramName = programName;
+        }
+
+        // read all Volunteer Programs
+        public List<User> ReadAllVolunteerPrograms()
+        {
+            DBservices dbs = new DBservices();
+            return dbs.ReadVolunteerPrograms();
+        }
+
+    }
+}

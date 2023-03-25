3bb8d092869117239bcedf09a4c1f948ae9f1f4e
Anat Avital <119490084+AnatAvital26@users.noreply.github.com>
2023-03-25 20:08:56 +0300

    הוספת עמוד גאווה סקריפט להוספת פוסט

---
+++ startbootstrap-FOA/startbootstrap-agency-gh-pages/js/AddPostReport.js
@@ -0,0 +1,59 @@
+﻿
+//סקריפט של בני להעלאת תמונות
+$(document).ready(function () {
+
+    $('#buttonUpload').on('click', function () {
+        var data = new FormData();
+        var files = $("#files").get(0).files;
+
+        // Add the uploaded file to the form data collection  
+        if (files.length > 0) {
+            for (f = 0; f < files.length; f++) {
+                data.append("files", files[f]);
+            }
+        }
+
+        api = "https://localhost:7109/api/";
+        imageFolder = "https://localhost:7109/images/";
+
+        // Ajax upload  
+        $.ajax({
+            type: "POST",
+            url: api,
+            contentType: false,
+            processData: false,
+            data: data,
+            success: showImages,
+            error: error
+        });
+
+        return false;
+    });
+
+});
+
+function showImages(data) {
+    console.log(data);
+
+    var imgStr = "";
+
+    if (Array.isArray(data)) {
+
+        for (var i = 0; i < data.length; i++) {
+            src = imageFolder + data[i];
+            imgStr += `<img src='${src}'/>`;
+        }
+
+    }
+
+    else { // just in case you have an api returning a single string
+        src = imageFolder + data;
+        imgStr = `<img src='${src}'/>`;
+    }
+
+    document.getElementById("ph").innerHTML = imgStr;
+}
+
+function error(data) {
+    console.log(data);
+}

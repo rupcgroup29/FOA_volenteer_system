9213247bb5d220f370d9acd4bc5f0d365834a7ad
Lotem <lotemwolb@gmail.com>
2023-04-28 18:15:42 +0300

    hour report (add,update) + add new Team
    הוספת צוות חדש.
    הוספת דיווח שעות, עדכון סטטוס של דיווח שעות, קריאה של כל דיווחי השעות

--- FOA_Server/Models/UpdatePostStatus.cs
+++ FOA_Server/Models/UpdatePostStatus.cs
@@ -5,12 +5,17 @@
         public int PostID { get; set; }
         public int PostStatus { get; set; }
         public int PostStatusManager { get; set; }
+        public int RemovalStatus { get; set; }
+        public int RemovalStatusManager { get; set; }
 
-        public UpdatePostStatus(int postID, int postStatus, int postStatusManager)
+
+        public UpdatePostStatus(int postID, int postStatus, int postStatusManager, int removalStatus, int removalStatusManager)
         {
             PostID = postID;
             PostStatus = postStatus;
             PostStatusManager = postStatusManager;
+            RemovalStatus = removalStatus;
+            RemovalStatusManager = removalStatusManager;
         }
 
     }
